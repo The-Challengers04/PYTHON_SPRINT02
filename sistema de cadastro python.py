8c04735da4089950e336617b6347e5bca981bbcc
anabeatrizfarah <biaafarah@gmail.com>
2023-04-12 18:09:14 -0300

    Sistema de cadastro de estabelecimentos

--- sistema de cadastro python.py
+++ sistema de cadastro python.py
@@ -7,13 +7,35 @@ def criarcadastro():
     logins = open('logins.txt','a')
     logins.write(f'-Nome: {nome} -Sobrenome: {sobrenome} -Email: {email} -senha: {senha} \n')
 
+def cadastrarnovo():
+    nomeEstabelecimento = input('Digite o nome do estabelecimento')
+    locEstabelecimento = input('Digiteo endereço do estabelecimento')
+    notaEstabelecimento = int(input(f'Digite uma nota, de 0 a 5, para a acessibilidade dentro do estabelecimento {nomeEstabelecimento}'))
+    if notaEstabelecimento >=4:
+        int(input('Ficamos felizes em saber disso! Gostaria de compartilhar isso no feed dos seus amigos? Digite 1 para sim e 2 para não'))
+    reviewEstabelecimento = input('Escreva uma Review do local para outros usuários')
+
+
+
+
 #menu
 while True:
-    escolha = input('Escolha uma opção: 1 - criar um cadastro; 2 - sair;')
-    if escolha == '1':
+    escolha1 = input('Escolha uma opção: 1 - criar um cadastro; 2 - sair;')
+    if escolha1 == '1':
         criarcadastro()
+        escolha2 = int(input("Bem vindo ao aplicativo Inclui+! Por onde deseja começar? \n 1- Cadastrar um novo estabelecimento 2- sair "))
+        if escolha2 == 1:
+            cadastrarnovo()
+        elif escolha2 == 2:
+            print("Obrigada por se cadastrar no App Inclui+!")
+            break
+        else:
+            print('Por favor escolha uma opção válida')
 
-    elif escolha == '2':
+
+
+
+    elif escolha1 == '2':
         print('Obrigada por acessar o site')
         break
 
