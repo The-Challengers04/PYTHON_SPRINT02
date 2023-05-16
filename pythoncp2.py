f1f402ef494b078f66772b0c01dee660b26dbbd9
biaafarah <rm97865@fiap.com.br>
2023-05-15 22:04:54 -0300

    comentarios

--- pythoncp2.py
+++ pythoncp2.py
@@ -1,31 +1,36 @@
 #funções utilizadas:
 #realizando o login:
-def criarcadastro():
-    nome = input('Digite seu nome ')
-    sobrenome = input('Digite seu sobrenome ')
-    email = input('Digite seu email ')
-    senha = input('Digite sua senha ')
+def criarcadastro(): #função
+    nome = input('Digite seu nome ') #pede nome
+    sobrenome = input('Digite seu sobrenome ') #pede sobrenome
+    email = input('Digite seu email ') #pede email
+    senha = input('Digite sua senha ') #pede senha
     logins = open('logins.txt','a') #abre um arquivo txt e coloca as informações do usuário
-    logins.write(f'-Nome: {nome} -Sobrenome: {sobrenome} -Email: {email} -senha: {senha} \n')
+    logins.write(f'-Nome: {nome} -Sobrenome: {sobrenome} -Email: {email} -senha: {senha} \n') #escreve no arquivo as informações coletadas
+
 #fazendo a review:
 def fazerreview():
-    nomeEstabelecimento = input('Digite o nome do estabelecimento')
-    locEstabelecimento = input('Digiteo endereço do estabelecimento')
-    notaEstabelecimento = int(input(f'Digite uma nota, de 0 a 5, para a acessibilidade dentro do estabelecimento {nomeEstabelecimento}'))
+    nomeEstabelecimento = input('Digite o nome do estabelecimento') #pede nome do estabelecimento
+    locEstabelecimento = input('Digiteo endereço do estabelecimento') #pede endereço do estabelecimento
+    notaEstabelecimento = int(input(f'Digite uma nota, de 0 a 5, para a acessibilidade dentro do estabelecimento {nomeEstabelecimento}')) #nota para o estabelecimento
     if notaEstabelecimento >=4: #se a nota for maior que 4, a pessoa pode compartilhar isso com seus amigos do app
         int(input('Ficamos felizes em saber disso! Gostaria de compartilhar isso no feed dos seus amigos? Digite 1 para sim e 2 para não'))
     reviewEstabelecimento = input('Escreva uma Review do local para outros usuários')
+
 #post na comunidade:
 def postcomunidade():
     tema = input('Qual o tema central do seu post?')
     post = input('O que você deseja falar?')
-    confirmação = input(f'Você deseja mesmo postar isso? Digite s para sim e n para não {post}')
+    confirmação = input(f'Você deseja mesmo postar isso? Digite s para sim e n para não {post}') #confirma se o usuário quer mesmo fazer o post
     if confirmação.lower() == 's':
         print('Postagem concluída')
     elif confirmação.lower() == 'n':
-        reconfirma = input('Reescreva sua postagem ou aperte 1 para sair')
+        reconfirma = input('Reescreva sua postagem ou aperte 1 para sair') #loop para o usuário reescrever o post, ou apertar 1 e apagar o post
         if reconfirma != 1:
+            reconfirma = post #o novo post salvo na variável post
             print('Postagem concluída')
+        else:
+            print('Post deletado') #se o usuário digitar 1, o post é deletado
 def cadastrarestabelecimento():
     nomeestabelecimento = input('Digite o nome do seu estabelecimento ')
     cnpj = input('Digite seu CNPJ')
@@ -42,8 +47,7 @@ def login():
         if email in valores[3] and senha in valores[4]:
             print('bem vindo')
         else:
-            print('usuario/senha incorretos')        
-
+            print('usuario/senha incorretos')    
 
 
 
@@ -58,7 +62,7 @@ while True:
         break
     else:
         print('Escolha uma opção valida')
-while valida == True:
+while True:
     menu = int(input('O que você deseja fazer hoje: \n 1- Fazer review de um estabelecimento \n 2- Fazer um post na comunidade \n 3- Cadastrar seu próprio estabelecimento \n 4- sair '))
     match menu:
         case 1:
